43e18972f44783296879a7eaf990284fd96c9ecf
Ziad Hossam <93387011+ZiadHossam74@users.noreply.github.com>
2023-03-25 17:16:40 +0200

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text("First Application"),
+      ),
+      body: Center(
+        child: Text(
+          "Ziad Hossam Alden",
+        ),
+      ),
+    ),
+  ));
+}
