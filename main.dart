c2249a4024e2c19a27f0da58e4e1ecd706be7bfe
Ziad Hossam <93387011+ZiadHossam74@users.noreply.github.com>
2023-05-06 12:13:47 +0300

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,107 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:toku/screens/home_page.dart';
 
-void main() => runApp(BusinessCardApp());
+void main() {
+  runApp(TokuApp());
+}
 
-class BusinessCardApp extends StatelessWidget {
+class TokuApp extends StatelessWidget {
+  const TokuApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Business Card App',
-      home: BusinessCard(),
+      debugShowCheckedModeBanner: false,
+      home:HomePage() ,
     );
   }
 }
 
-class BusinessCard extends StatelessWidget {
-  final String name = 'Ziad Hossam Eldeen';
-  final String job = 'AI Developer';
-  final String email = 'zh112@fayoum.edu.eg';
-  final String phone = '+20 0101968149';
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Business Card'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          CircleAvatar(
-            radius: 50.0,
-            backgroundImage: NetworkImage('images/IMG_20210111_170504_229.jpg'),
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
-          Text(
-            name,
-            style: TextStyle(
-              fontSize: 22.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
-          Text(
-            job,
-            style: TextStyle(
-              fontSize: 22.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Icon(
-                Icons.email,
-                color: Colors.grey[600],
-              ),
-              SizedBox(
-                width: 10.0,
-              ),
-              Container(
-                //width: 200.0,
-                child: Text(
-                  email,
-                  style: TextStyle(
-                    fontSize: 16.0,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Icon(
-                Icons.phone,
-                color: Colors.grey[600],
-              ),
-              SizedBox(
-                width: 10.0,
-              ),
-              Container(
-                //width: 250.0,
-                child: Text(
-                  phone,
-                  style: TextStyle(
-                    fontSize: 16.0,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          
-        ],
-      ),
-    );
-  }
-}
